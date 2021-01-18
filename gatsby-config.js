2d69752f4e206d415171a28f11141ec8b0a4fca4
Kevin Wang <kev.wang2@gmail.com>
2021-01-18 12:53:39 -0500

    Remove google analytics package since gtag is used instead

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,19 +30,6 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: process.env.GA_TRACKING_ID,
-    //     head: false,
-    //     anonymize: true,
-    //     respectDNT: true,
-    //     pageTransitionDelay: 0,
-    //     defer: false,
-    //     sampleRate: 5,
-    //     siteSpeedSampleRate: 10,
-    //   },
-    // },
     {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
@@ -50,14 +37,6 @@ module.exports = {
         trackingIds: [
           process.env.GA_TRACKING_ID, // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
