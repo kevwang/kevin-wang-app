00afb51fd937dfb82049f3b480bef8f1fd20693f
Kevin Wang <kev.wang2@gmail.com>
2021-01-22 11:51:19 -0500

    Fixed ToC scaling behavior, adjusted prismjs css, and hard-coded OMSCS links

--- src/components/post-content.js
+++ src/components/post-content.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Container from '../components/container'
 import TOC from '../components/toc'
 
-export default function PostContent({post}) {
+export default function PostContent({post, children}) {
     return (
       <Container>
         <div className="wrapper">
           <div className="section-headline">
             <h1>{post.title}</h1>
           </div>
+          { children }
           <div className='post-content'
             dangerouslySetInnerHTML={{
               __html: post.body.childMarkdownRemark.html,
--- src/pages/omscs.js
+++ src/pages/omscs.js
@@ -6,6 +6,29 @@ import HeroImage from '../components/hero-image'
 import Root from '../components/root'
 import Feed from '../components/feed'
 import PostContent from '../components/post-content'
+import { Link } from 'gatsby'
+
+const courses = [
+  ['cs-6290-high-performance-computer-architecture', 'CS 6290 - High Performance Computer Architecture', '🖥️'],
+  ['cs-6420-bayesian-statistics', 'CS 6420 - Bayesian Statistics', '🔢'],
+  ['cs-6476-computer-vision', 'CS 6476 - Computer Vision', '👁️'],
+  ['cs-6515-graduate-algorithms', 'CS 6515 - Graduate Algorithms','🎓'],
+  ['cs-6601-artificial-intelligence', 'CS 6601 - Artificial Intelligence','🧠'],
+  ['cs-6644-simulation', 'CS 6644 - Simulation','📊'],
+  ['cs-7638-ai-for-robotics', 'CS 7638 - AI for Robotics','🤖'],
+  ['cs-7642-reinforcement-learning', 'CS 7642 - Reinforcement Learning','🧠'],
+  ['cs-7646-machine-learning-for-trading', 'CS 7646 - Machine Learning for Trading','📈'],
+  ['cs-8803-ai-ethics-and-society', 'CS 8803 - AI, Ethics, and Society','👪'],
+  ['cse-6242-data-and-visual-analytics', 'CSE 6242 - Data and Visual Analytics','📊'],
+]
+
+const allCourseLinks = (
+  <div>
+  {courses.map((courseTuple) => {
+    return (<>{courseTuple[2]}<Link to={courseTuple[0]}>{courseTuple[1]}</Link><br/></>)
+  })}
+  </div>
+)
 
 export default function BlogPostTemplate(props) {
     const allPosts = get(props, 'data.allContentfulBlogPost.edges')
@@ -16,7 +39,10 @@ export default function BlogPostTemplate(props) {
       <Root sidebarOpen={false}>
         <Helmet title={'OMSCS Course Review'} />
         <HeroImage post={post}/>
-        <PostContent post={post} />
+        <PostContent post={post}>
+          {allCourseLinks}
+          {/* <Link to='omscs/cs-6476-computer-vision'>CS 6476 - Computer Vision</Link> */}
+        </PostContent>
         <Feed allPosts={allPosts}/>
       </Root>
     )
